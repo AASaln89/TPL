c4dcd376a136c51d714e2675bd023ef0ca285732
Andrey Salnikov <kvanta13@yandex.ru>
2022-01-06 23:19:22 +0300

    123

--- TPL/Program.cs
+++ TPL/Program.cs
@@ -11,16 +11,16 @@ namespace TPL
         static void Main(string[] args)
         {
             Console.WriteLine("Введите размер массива");
-            int n = Convert.ToInt32(Console.ReadLine());
+            object n = Convert.ToInt32(Console.ReadLine());
             //
             Func<object, int[]> func1 = new Func<object, int[]>(GetArray);
-            Task<int[]> task1 = new Task<int[]>(func1, 6);
+            Task<int[]> task1 = new Task<int[]>(func1, n);
             ///
-            Action<Task<int[]>> func2 = new Action<Task<int[]>>(SummArray);
+            Func<Task<int[]>, int> func2 = new Func<Task<int[]>, int>(SummArray);
             Task task2 = task1.ContinueWith(func2);
             ////
-            Action<Task<int[]>> action = new Action<Task<int[]>>(MaxArray);
-            Task task3 = task2.ContinueWith(func1);
+            Func<Task<int[]>, int> func3 = new Func<Task<int[]>, int>(MaxArray);
+            Task task3 = task1.ContinueWith(func3);
             /////
             task1.Start();
             Console.ReadKey();
@@ -28,34 +28,42 @@ namespace TPL
         static int[] GetArray(object m)
         {
             int n = (int)m;
-            int[] array = new int[6];
+            int[] array = new int[n];
             Random random = new Random();
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < n; i++)
             {
                 array[i] = random.Next(0, 10);
+                Console.Write(array[i]);
             }
             return array;
         }
-        static void SummArray(Task<int[]> task)
+        static int SummArray(Task<int[]> task)
         {
             int[] array = task.Result;
             int sumarray = 0;
-            for (int i = 0; i < 6 - 1; i++)
+            for (int i = 0; i < array.Count(); i++)
             {
-                sumarray += array[i];
-                Console.Write($"{array[i]} ");
+                sumarray = sumarray + array[i];
             }
+            Console.WriteLine("SUM");
+            Console.WriteLine("{0}", sumarray);
+            return sumarray;
         }
-        static void MaxArray(Task<int[]> task)
+        static int MaxArray(Task<int[]> task)
         {
             int[] array = task.Result;
             int max = array[0];
             foreach (int a in array)
             {
                 if (a > max)
+                {
                     max = a;
-                Console.Write($"{array[max]} ");
+                }
             }
+            Console.WriteLine();
+            Console.WriteLine("MAX");
+            Console.WriteLine(max);
+            return max;
         }
     }
 }
